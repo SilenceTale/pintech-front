a6443039519850dd1375f38acaab42300fef6d04
SilenceTale <chomn55@gmail.com>
2025-02-07 16:50:51 +0900

    0

--- src/app/global/container/WithUserContainer.tsx
+++ src/app/global/container/WithUserContainer.tsx
@@ -1,9 +1,18 @@
 'use client'
 import useUser from '../hooks/useUser'
 import LoginContainer from '@/app/member/containers/LoginContainer'
+import { MainContentBox } from '../components/ContentBox'
+import { MainTitle } from '../components/StyledTitle'
 
-export default function WithUserContainer(UserContainer) {
+export default function WithUserContainer(UserContainer, redirectUrl) {
   const { isLogin } = useUser()
 
-  return isLogin ? <UserContainer /> : <LoginContainer />
+  return isLogin ? (
+    <UserContainer />
+  ) : (
+    <MainContentBox max={450} min={350}>
+      <MainTitle>로그인</MainTitle>
+      <LoginContainer redirectUrl={redirectUrl} />
+    </MainContentBox>
+  )
 }
--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import MypageContainer from './containers/MypageContainer'
 import WithUserContainer from '../global/container/WithUserContainer'
