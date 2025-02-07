95ae9c94437f21a6a3db37297ae084a6fa3e3476
SilenceTale <chomn55@gmail.com>
2025-02-07 16:45:09 +0900

    오타 수정

---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import MypageContainer from './containers/MypageContainer'
+import WithUserContainer from '../global/container/WithUserContainer'
+
+const Mypage = () => {
+  return WithUserContainer(MypageContainer)
+}
+
+export default React.memo(Mypage)
